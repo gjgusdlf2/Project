f589ffedb5da1f374cbe7a1b9f86b9867ca12aa4
1027 <1027@hb1027>
2019-05-16 20:04:03 +0900

    project 05/16

---
+++ phoneshop/src/main/java/com/phoneshop/web/cmm/Proxy.java
@@ -0,0 +1,85 @@
+package com.phoneshop.web.cmm;
+
+import java.util.Map;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data @Component @Lazy
+public class Proxy {
+	private int pageNum,pageSize,blockSize,startRow,
+			endRow,startPage,endPage,prevBlock,nextBlock,totalCount;
+	private String search;
+	private boolean existPrev, existNext;
+	
+	public void carryOut(Map<?,?> paramMap) {
+		int pageNum;
+		String _pageNum = (String)paramMap.get("page_num");
+		if(_pageNum == null) {
+			pageNum = 1;
+		}else {
+			pageNum = Integer.parseInt(_pageNum);
+		}
+		
+		int pageSize;
+		String _pagesize = (String)paramMap.get("page_size");
+		if(_pagesize == null) {
+			pageSize = 10;
+		}else {
+			pageSize = Integer.parseInt(_pagesize);
+		}
+		
+		int blockSize;
+		String _blocksize = (String)paramMap.get("block_size");
+			if(_blocksize == null) {
+					blockSize = 5;
+				}else {
+					blockSize = Integer.parseInt(_blocksize);
+				}
+		
+		totalCount = (int)paramMap.get("total_count");
+		int nmg = totalCount % pageSize;
+		int pageCount;
+			if(nmg == 0) {
+				pageCount = totalCount / pageSize;
+			}else {
+				pageCount = totalCount / pageSize + 1;
+			}
+		startRow = (pageNum -1) * pageSize;
+		int endRow;
+			if(totalCount > pageNum * pageSize) {
+				endRow = pageNum * pageSize;
+				}else {
+				endRow = totalCount;
+			}
+		int blockNum = (pageNum-1)/blockSize;
+			if(existPrev) {
+				startPage = blockNum/blockSize;
+			}else {
+				startPage = 1;
+			}
+		endPage = startPage + (blockSize-1);
+		startPage = pageNum - ((pageNum-1) % blockSize);
+		endPage = startPage + (blockSize - 1);
+			if(endPage>pageCount) {
+				endPage = pageCount;
+			}
+		existPrev = (startPage - pageSize) > 0;
+		existNext = (startPage + pageSize) <= pageCount;
+		prevBlock = startPage - pageSize;
+		nextBlock = startPage + pageSize;
+		search = (String)paramMap.get("search");
+		System.out.println("스타트로우 :"+startRow);
+		System.out.println("엔드로우 :"+endRow);
+		System.out.println("토탈카운트 :"+totalCount);
+		System.out.println("스타트페이즈 :"+startPage);
+		System.out.println("엔드페이지 :"+endPage);
+		System.out.println("프리블록 :"+prevBlock);
+		System.out.println("넥스트블록 :"+nextBlock);
+		System.out.println("페이지카운트 :"+pageCount);
+		System.out.println("페이지넘 :"+pageNum);
+		System.out.println("블록사이즈 :"+blockSize);
+	}
+}
